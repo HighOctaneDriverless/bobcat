354b99abd8600e96067ae952b7ec281b7848cb5d
Martin Mayr <martinmayr@gmx.de>
2018-01-31 18:59:14 +0100

    script fixed

--- run_track_ackermann.sh
+++ run_track_ackermann.sh
@@ -8,6 +8,7 @@ x-terminal-emulator -e "roslaunch bobcat_gazebo track_world.launch" #--noclose
 sleep 10
 x-terminal-emulator -e "roslaunch bobcat_control bobcat_control.launch" #--noclose
 sleep 3
-x-terminal-emulator -e "rosrun rqt_gui rqt_gui" #--noclose
-sleep 3
 x-terminal-emulator -e "rosrun bobcat_ackermann ackermann.py"
+sleep 3
+x-terminal-emulator -e "rosrun rqt_gui rqt_gui" #--noclose
+
