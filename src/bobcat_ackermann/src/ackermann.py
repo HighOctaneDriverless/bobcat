065df91690b41eccc6b361974ffb228df1058c44
Martin Mayr <martinmayr@gmx.de>
2018-03-02 23:23:39 +0100

    pfwl working

--- src/bobcat_ackermann/src/ackermann.py
+++ src/bobcat_ackermann/src/ackermann.py
@@ -8,12 +8,12 @@ import math
 
 class Ackermann():
     def __init__(self):
-	self.subscribe()
+        self.subscribe()
         #acceleration | speed
         self.desired_speed = Float64(0.0)
-	self.measured_speed = 0.0
-	self.speed_out = Float64(0.0)        
-	#value between -1.0 and 1.0 | straight 0.0
+        self.measured_speed = 0.0
+        self.speed_out = Float64(0.0)
+        #value between -1.0 and 1.0 | straight 0.0
         self.steering = 0.0
         self.steering_left = 0.0
         self.steering_right = 0.0
@@ -40,15 +40,15 @@ class Ackermann():
         self.set_speed(data.data)
 
     def callback_state(self, data):
-	x = data.twist[-1].linear.x
-	y = data.twist[-1].linear.y
-	if x+ y > 0.0:
-		self.measured_speed = math.sqrt(x**2 + y**2) 
-	else:
-		self.measured_speed = - math.sqrt(x**2 + y**2) 
+        x = data.twist[-1].linear.x
+        y = data.twist[-1].linear.y
+        if x+ y > 0.0:
+            self.measured_speed = math.sqrt(x**2 + y**2)
+        else:
+            self.measured_speed = - math.sqrt(x**2 + y**2)
 
     def callback_controller(self,data):
-	self.set_speed(data.axes[1]/2)
+        self.set_speed(data.axes[1]/2)
         self.set_steering(data.axes[0])
         rospy.loginfo("Joystick speed "+ str(data.axes[1]/2))
 
@@ -63,33 +63,33 @@ class Ackermann():
         # init subscriber
         rospy.Subscriber("/bobcat/ackermann_steer/command", Float64, self.callback_steering)
         rospy.Subscriber("/bobcat/ackermann_speed/command", Float64, self.callback_speed)
-	rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback_state)
+        rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback_state)
         #rospy.Subscriber("joy", Joy, self.callback_controller)
         #perhaps important. has to be checked
         #rospy.spin()
 
     def pid(self):
-	p = 2.0 # 3 as initial guess :D i and d missing for now needs to be determined how much that is needed
-	#print "desired_speed : " + repr(self.desired_speed) + "   measured_speed: " + repr(self.measured_speed)
-	#print "measured_speed: " 
-	#print type(self.measured_speed)
-	#print "desired_speed: " 
-	print type(self.desired_speed.data)
-	diff =  self.desired_speed.data - self.measured_speed
+        p = 2.0 # 3 as initial guess :D i and d missing for now needs to be determined how much that is needed
+        #print "desired_speed : " + repr(self.desired_speed) + "   measured_speed: " + repr(self.measured_speed)
+        #print "measured_speed: "
+        #print type(self.measured_speed)
+        #print "desired_speed: "
+        #print type(self.desired_speed.data)
+        diff =  self.desired_speed.data - self.measured_speed
 
-	self.speed_out = diff * p
-	if (self.speed_out > 0.6):
-		self.speed_out = 0.6
-	elif (self.speed_out < -0.6):
-		self.speed_out = -0.6
+        self.speed_out = diff * p
+        if (self.speed_out > 0.6):
+            self.speed_out = 0.6
+        elif (self.speed_out < -0.6):
+            self.speed_out = -0.6
 
     def publish(self):
-	self.pid()
+        self.pid()
         self.pub_acc_left.publish(self.speed_out)
         self.pub_acc_right.publish(self.speed_out)
         self.pub_steering_left.publish(self.steering)
         self.pub_steering_right.publish(self.steering)
-        rospy.loginfo("Speed: "+str(self.speed_out)+"Steer: "+str(self.steering))
+        #rospy.loginfo("Speed: "+str(self.speed_out)+"Steer: "+str(self.steering))
 
 
 def main():
@@ -97,7 +97,7 @@ def main():
     rospy.loginfo("Ackermann started")
     rate = rospy.Rate(100) # 
     while not rospy.is_shutdown():
-        
+
         ackerm.publish()
         rate.sleep()
 
--- src/bobcat_pfwl/src/pfwl.py
+++ src/bobcat_pfwl/src/pfwl.py
@@ -7,12 +7,13 @@ from sensor_msgs.msg import LaserScan
 class PFWL():
     def __init__(self):
         #acceleration | speed
-        self.speed = 0.3
+        self.speed = 10.0
+        #self.speed = 0.0
         #value between -1.0 and 1.0 | straight 0.0
         self.steering = 0.0
 
         #CONSTANTS from model
-        self.alpha = 30.0
+        self.alpha = 9.0
 
         #scans
         self.left_dist = 1.0
@@ -26,29 +27,48 @@ class PFWL():
         self.ack_speed = rospy.Publisher("/bobcat/ackermann_speed/command", Float64, queue_size=1)
         self.ack_steer = rospy.Publisher("/bobcat/ackermann_steer/command", Float64, queue_size=1)
 
+        rospy.Subscriber("/scan",LaserScan,self.callback)
+
     def callback(self,scan):
         self.scan = scan
 
+        left = 1081/5*2
+        leftleft = 1081/4
+        right = 1081/5*3
+        rightright = 1081*3/4
+        middle = 1081/2
+
         #OLD CODE
-        # self.left_dist = scan.ranges[1080/5*3]
+        # self.left_dist = scan.ranges[right]
         # if self.left_dist == float('inf'):
         #     self.left_dist = 10.0
         # elif self.left_dist < 0.1:
         #     self.left_dist = 0.1
-        # self.middle_dist = scan.ranges[1080/2]
+        # self.middle_dist = scan.ranges[middle]
         # if self.middle_dist == float('inf'):
         #     self.middle_dist = 10.0
         # elif self.middle_dist < 1.0:
         #     self.middle_dist = 1.0
-        # self.right_dist = scan.ranges[1080/5*2]
+        # self.right_dist = scan.ranges[left]
         # if self.right_dist == float('inf'):
         #     self.right_dist = 10.0
         # elif self.right_dist < 0.1:
         #     self.right_dist = 0.1
 
+        rate = scan.ranges[leftleft]/scan.ranges[rightright]
+        if rate < 1:
+            self.steering = -(scan.angle_increment*leftleft+scan.angle_min)
+            rospy.loginfo("left")
+        else:
+            self.steering = -(scan.angle_increment*rightright+scan.angle_min)
+            rospy.loginfo("right")
+
+        self.set_steering(self.steering)
+
+        rospy.loginfo("Winkel (rad): "+str(self.steering))
         #rospy.loginfo((len(scan.ranges), min(scan.ranges)))
         #rospy.loginfo("left "+str(scan.ranges[1080/2])+" array number: "+str(1081/4))
-        rospy.loginfo("left: "+str(self.left_dist)+" right: "+ str(self.right_dist) + "s_L: " + str(1080/5*2)+ " s_R: "+str(1080/5*3))
+        #rospy.loginfo("left: "+str(self.left_dist)+" right: "+ str(self.right_dist) + "s_L: " + str(1080/5*2)+ " s_R: "+str(1080/5*3))
 
     def set_speed(self, new_speed):
         self.speed = new_speed
@@ -58,36 +78,32 @@ class PFWL():
 
     def decideSteering(self):
         rate = self.right_dist/self.left_dist
-        if rate == 0.0:
-            rate = 0.1
-        if self.left_dist < self.right_dist:
-            self.steering = rate / self.alpha * (-1)
-            #self.steering = (self.right_dist - self.left_dist)/self.alpha
+        if rate == 1.0:
+            self.set_steering = 0.0
+        if rate<1:
+            diff = self.left_dist - self.right_dist
+            self.steering = diff / self.alpha
+            if self.steering > 1.5:
+                self.steering = 1.5
         else:
-            rate = 1/rate
-            self.steering = rate / self.alpha
-            #self.steering = (self.left_dist-self.right_dist)/self.alpha * (-1)
-
+            diff = self.right_dist - self.left_dist
+            self.steering = diff / self.alpha * (-1)
+            if self.steering < -1.5:
+                self.steering = -1.5
         rospy.loginfo("steering: "+str(self.steering))
 
-    def subscribe(self):
-        # init subscriber
-        rospy.Subscriber("/scan",LaserScan,self.callback)
-        #perhaps important. has to be checked
-        #rospy.spin()
-
     def publish(self):
-        self.ack_speed.publish(self.speed)
-        self.ack_steer.publish(self.steering)
+        self.ack_speed.publish(Float64(self.speed))
+        self.ack_steer.publish(Float64(self.steering))
 
 def main():
     pfwl = PFWL()
     rospy.loginfo("PFWL started")
-    rate = rospy.Rate(10) # 100hz
+    rate = rospy.Rate(20) # 100hz
     i = 0
     while not rospy.is_shutdown():
-        pfwl.subscribe()
-        pfwl.decideSteering()
+        #pfwl.subscribe()
+        #pfwl.decideSteering()
         pfwl.publish()
         #report decisions
         if i%5:
