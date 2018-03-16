612adb0b61c0f689125f266bd6c8070722b1c355
Martin Mayr <martinmayr@gmx.de>
2018-03-16 17:36:08 +0100

    asd

--- src/bobcat_pfwl/src/camera_view.py
+++ src/bobcat_pfwl/src/camera_view.py
@@ -2,6 +2,27 @@
 
 import rospy
 from sensor_msgs.msg import Image
+import cv2
+import numpy as np
 
 class Camera_view():
+	def __init__(self):
+		rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+		#self.image = 
 
+	def callback(self,image):
+		np_arr = np.fromstring(image.data, np.uint8)
+		print(image.encoding)
+		#self.image = np.arr.reshape(())
+
+
+def main():
+	cam = Camera_view()
+	while not rospy.is_shutdown():
+		pass
+
+if __name__ == '__main__':
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
