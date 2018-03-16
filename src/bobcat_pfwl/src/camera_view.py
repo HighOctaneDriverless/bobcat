144193874c22884418275e1d9fe35e7da6583582
Martin Mayr <martinmayr@gmx.de>
2018-03-16 18:02:51 +0100

    camera_view working

--- src/bobcat_pfwl/src/camera_view.py
+++ src/bobcat_pfwl/src/camera_view.py
@@ -7,19 +7,30 @@ import numpy as np
 
 class Camera_view():
 	def __init__(self):
+		#init ros_node
+        	rospy.init_node('pfwl', anonymous=True)
+
 		rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
-		#self.image = 
+		self.image = np.zeros((480,640,3)) 
 
 	def callback(self,image):
 		np_arr = np.fromstring(image.data, np.uint8)
-		print(image.encoding)
-		#self.image = np.arr.reshape(())
+		self.image = np_arr.reshape((480,640,3))
+		self.image = self.image[...,::-1]	
+		cv2.imshow('Image',self.image)
+		cv2.waitKey(1)
+
+	def show(self):
+		cv2.imshow(self.image)
 
 
 def main():
 	cam = Camera_view()
+	rospy.loginfo("cam infos started")
+	rate = rospy.Rate(1)
 	while not rospy.is_shutdown():
-		pass
+		#cam.show()
+		rate.sleep()
 
 if __name__ == '__main__':
 	try:
