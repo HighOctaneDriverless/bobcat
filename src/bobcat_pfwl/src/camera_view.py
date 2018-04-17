e7940198e673fa618a2b174e35faf16e0062ca25
Martin Mayr <martinmayr@gmx.de>
2018-04-17 18:16:12 +0200

    added py for cam

--- src/bobcat_pfwl/src/camera_view.py
+++ src/bobcat_pfwl/src/camera_view.py
@@ -4,12 +4,14 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2
 import numpy as np
+import sys
 
 class Camera_view():
 	def __init__(self):
 		#init ros_node
         	rospy.init_node('pfwl', anonymous=True)
 
+		self.timedelta = 0
 		rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
 		self.image = np.zeros((480,640,3)) 
 
@@ -22,14 +24,24 @@ class Camera_view():
 
 	def show(self):
 		cv2.imshow(self.image)
+		#cv2.imwrite("")
+	
+	def write(self):
+		try:
+			filename = "../pics/image_t"+str(self.timedelta)+".png"
+			cv2.imwrite(filename,self.image)
+			self.timedelta = self.timedelta + 1
+			print("file saved")
+		except: 
+			print("error",sys.exc_info()[0])
 
 
 def main():
 	cam = Camera_view()
 	rospy.loginfo("cam infos started")
-	rate = rospy.Rate(1)
+	rate = rospy.Rate(0.5)
 	while not rospy.is_shutdown():
-		#cam.show()
+		cam.write()
 		rate.sleep()
 
 if __name__ == '__main__':
