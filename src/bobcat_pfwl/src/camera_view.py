db34501126f6d88fd7660157ca5bec30f69828bd
Martin Mayr <martinmayr@gmx.de>
2018-04-23 18:24:41 +0200

    added canny

--- src/bobcat_pfwl/src/camera_view.py
+++ src/bobcat_pfwl/src/camera_view.py
@@ -13,15 +13,27 @@ class Camera_view():
 		rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
 		self.image = np.zeros((480,640,3)) 
 
+		#canny parameters
+		self.ratio = 3
+		self.kernel_size = 3
+		self.threshold = 50 # between 0 and 100
+
 	def callback(self,image):
 		np_arr = np.fromstring(image.data, np.uint8)
 		self.image = np_arr.reshape((480,640,3))
-		self.image = self.image[...,::-1]	
+		self.image = self.image[...,::-1]
+		self.canny()
 		cv2.imshow('Image',self.image)
 		cv2.waitKey(1)
 
 	def show(self):
 		cv2.imshow(self.image)
+
+	def canny(self):
+		img_gray = cv2.cvtColor(self.image,cv2.COLOR_RGB2GRAY)
+		img_gray = cv2.blur(img_gray,(3,3))
+		edges = cv2.Canny(img_gray,self.threshold,self.threshold*self.ratio,self.kernel_size)
+		cv2.imshow("Canny",self.edges)
 
 
 def main():
